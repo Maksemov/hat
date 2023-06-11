6d92abd5a5d4ba16973bc3b89668e56dd09d8151
Maxem <Maxim.Tsyrkunov@vistar.su>
2023-06-11 14:40:08 +0300

    sfewfwef

--- server/server.js
+++ server/server.js
@@ -35,11 +35,11 @@ const getRandomPerson = () => {
 const api = '/api'
 
 const app = express();
-app.use(cors({
-  origin: ['https://hat-api.vercel.app'],
-  methods: ['POST', 'GET'],
-  credentials: true,
-}))
+// app.use(cors({
+//   origin: ['https://hat-api2.vercel.app/'],
+//   methods: ['POST', 'GET'],
+//   credentials: true,
+// }))
 app.use(bodyParser.json());
 
 
@@ -53,39 +53,39 @@ app.get('/', (req, res) => {
   res.send('Привет!');
 });
 
-app.get(api, (req, res) => {
-  res.send('Сука');
-});
+// app.get(api, (req, res) => {
+//   res.send('Сука');
+// });
 
-app.get(`${api}/person`, (req, res) => {
-  const person = getRandomPerson();
-  if (person === -1) {
-    res.sendFile(path.resolve(__dirname, 'index.html'));
-  } else {
-    res.send(`${person}`);
+// app.get(`${api}/person`, (req, res) => {
+//   const person = getRandomPerson();
+//   if (person === -1) {
+//     res.sendFile(path.resolve(__dirname, 'index.html'));
+//   } else {
+//     res.send(`${person}`);
 
-  }
-});
+//   }
+// });
 
-app.get(`${api}/persons`, (req, res) => {
+// app.get(`${api}/persons`, (req, res) => {
 
-    res.send(persons);
-});
+//     res.send(persons);
+// });
 
-app.post(`${api}/persons`, (req, res) => {
-  const message = req.body;
-  console.log(message);
-  persons = [...persons, ...message];
-  res.send(message);
-});
+// app.post(`${api}/persons`, (req, res) => {
+//   const message = req.body;
+//   console.log(message);
+//   persons = [...persons, ...message];
+//   res.send(message);
+// });
 
-app.post(`${api}/update`, (req, res) => {
-  const message = req.body.message;
-  playPersons = [...persons]
-  const person = getRandomPerson();
-  console.log(`Received message: ${message}`);
-  res.send(`${person}`);
-});
+// app.post(`${api}/update`, (req, res) => {
+//   const message = req.body.message;
+//   playPersons = [...persons]
+//   const person = getRandomPerson();
+//   console.log(`Received message: ${message}`);
+//   res.send(`${person}`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
